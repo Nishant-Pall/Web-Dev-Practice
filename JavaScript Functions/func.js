cd1902a50a3337ac7076374898def6b9191e6dba
nishant <palln6935@gmail.com>
2020-05-12 02:12:55 +0530

    isEven function

--- JavaScript Functions/func.js
+++ JavaScript Functions/func.js
@@ -8,3 +8,10 @@ square(10);
 var capitalize = function(str){
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+function isEven(num){
+	if(num % 2 == 0){
+		return true;
+	}
+	return false;
+}
