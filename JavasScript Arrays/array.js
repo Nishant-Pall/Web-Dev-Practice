aa51222dbc7eb009857a7a093f00f4e30443d576
nishant <palln6935@gmail.com>
2020-05-13 15:21:20 +0530

    added javascript arrays

---
+++ JavasScript Arrays/array.js
@@ -0,0 +1,16 @@
+var friends = ['Charlie','Liz','David','Mattias'];
+
+friends[0] = 'Chuck';
+friends[1] = 'Lizzy';
+
+// adding new data
+
+friends[4] = 'Rachel';
+
+var friend = new Array();
+
+friends.length  // will give the length of the array
+
+// Arrays can hold any data type
+
+var random_collection = [49,true,'Hermoine', null];
