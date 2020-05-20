bb8998c6c7f1000b1b91db5a82210f1a1b379c48
nishant <palln6935@gmail.com>
2020-05-20 13:16:42 +0530

    added push(),pop()

---
+++ JavasScript Arrays/array_Methods.js
@@ -0,0 +1,13 @@
+var colors = ["red","orange","yellow"];
+colors.push("green");
+
+// ["red","orange","yellow","green"]
+
+colors.pop();
+
+// ["red","orange","yellow"]
+
+// pop() returns the removed statement
+
+var col = colors.pop();
+// col = yellow
