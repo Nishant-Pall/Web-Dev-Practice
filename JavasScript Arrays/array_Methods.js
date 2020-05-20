f03f8f0efe53b8ce3fc5fb0b5426cc5dec094837
nishant <palln6935@gmail.com>
2020-05-20 13:48:01 +0530

    added indexOf, and slice

--- JavasScript Arrays/array_Methods.js
+++ JavasScript Arrays/array_Methods.js
@@ -19,3 +19,23 @@ colors.shift();
 // shift() also returns removed element
 
 var colo = colors.shift();
+
+var friends = ["charlie","Liz","David","Mattias","Liz"];
+
+friends.indexOf("David"); //2
+
+friends.indexOf("Liz"); // 1, not 4
+
+// returns -1 if element isn't present
+
+
+
+var fruits = ['Banana','Orange','Lemon','Apple','Mango'];
+
+var citrus = fruits.slice(1,3);
+// citrus = ['Orange','Lemon']
+
+// You can use slice() to copy an entire array
+
+var nums = [1,2,3];
+var otherNums = nums.slice();
