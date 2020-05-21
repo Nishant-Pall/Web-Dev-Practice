f027209aca1e06a8e2ece2a8ee8ed544f4708820
nishant <palln6935@gmail.com>
2020-05-21 12:39:04 +0530

    updated todo list

--- JavasScript Arrays/list.js
+++ JavasScript Arrays/list.js
@@ -5,12 +5,23 @@ var input = prompt("What would you like to do?");
 while(input !== "quit"){
 
 	if(input == "list"){
-		console.log(todos);
+		todos.forEach(function(todo, index){
+			console.log("*********");
+			console.log(index + ":" + todo);
+			console.log("*********");
+		});
 	} else if(input == "new"){
 		// Ask for a new todo, and add it to todos array
 		var newTodo = prompt("Enter new Todo");
 		todos.push(newTodo);
-	}	
+		console.log("Added to Todo");
+	} else if(input === "delete"){
+		// ask for index of todo to be delted
+		var index = prompt("Enter index of todo to delete");
+		// delete that todo
+		todos.splice(index, 1);
+		console.log("Deleted Todo");
+	}
 
 	var input = prompt("What would you like to do?");
 }
