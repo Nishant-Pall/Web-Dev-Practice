968b342209f12e5e2baeee7a5aebec2206e8c84b
nishant <palln6935@gmail.com>
2020-05-25 20:38:35 +0530

    added DOM selectors

---
+++ Dom Manipulation/selectors.js
@@ -0,0 +1,14 @@
+// Selecting by id
+var tag = document.getElementById("highlight");
+
+// returns list of elements having same class
+var tags = document.getElementsByClassName("bolded");
+
+// returns elements by tag name
+var tags = document.getElementsByTagName("li");
+
+var body = document.getElementsByTagName("body");  
+
+// select by ID
+var tag = document.querySelector("#highlight"); 
+var tags = document.querySelector(".bolded");
