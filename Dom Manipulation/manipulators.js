f6cc0f2a808e9b3e36307e47fab2904916c3b1e2
nishant <palln6935@gmail.com>
2020-05-27 10:49:44 +0530

    added features of classList

--- Dom Manipulation/manipulators.js
+++ Dom Manipulation/manipulators.js
@@ -2,3 +2,9 @@ var h1 = document.querySelector("h1");
 
 h1.style.color = "yellow";
 h1.style.border = "5px solid purple";
+
+var p = document.querySelector("p");
+
+p.classList.add("big");
+
+// p.classList.toggle("big");
