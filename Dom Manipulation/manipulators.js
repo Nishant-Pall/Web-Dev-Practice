f6b9f120e0401a21561667f20492ddeac1f99e8c
nishant <palln6935@gmail.com>
2020-05-27 11:07:40 +0530

    added textContent, innerHTML

--- Dom Manipulation/manipulators.js
+++ Dom Manipulation/manipulators.js
@@ -6,5 +6,17 @@ h1.style.border = "5px solid purple";
 var p = document.querySelector("p");
 
 p.classList.add("big");
-
 // p.classList.toggle("big");
+
+// textContent
+var ul = document.querySelector("ul");
+
+// Retrive the textContent:
+// ul.textContent;
+
+// ul.textContent = "blah blah blah"
+
+// innerHTML
+p.innerHTML;
+
+ul.innerHTML = "Plants are awesome"; 
