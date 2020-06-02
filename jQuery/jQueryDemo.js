645a3645db7f39170fbbcf03cef630f0a2940596
nishant <palln6935@gmail.com>
2020-06-02 20:34:38 +0530

    more selectors

--- jQuery/jQueryDemo.js
+++ jQuery/jQueryDemo.js
@@ -5,3 +5,26 @@ $('#trigger').click(function(){
 	$('body').css('background','yellow');
 
 });
+
+$('h1').css('color','steelblue');
+
+$('li').css('color','blue');
+
+$('ui li a').css('color','red');
+
+$('#adorable').css('border','2px solid red');
+
+// we can also pass an object to change styles
+var styles = {
+	backgroundColor :'pink',
+	fontWeight : 'bold'
+};
+
+$('#adorable').css(styles);
+
+// in place objects
+
+$('li').css({
+	fontSize:'40px',
+	border:'3px dashed purple'
+});
