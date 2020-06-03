333af3fc4fb4c0f368dd11e746bd81beaf6a152b
nishant <palln6935@gmail.com>
2020-06-03 19:19:05 +0530

    jQuery Selectors

---
+++ jQuery/methods.js
@@ -0,0 +1,17 @@
+$('ul').text();
+
+$('h1').text('New Text!!');
+
+$('li').text("Dog's are adorable");
+
+$('ul').html();
+
+$('ul').html("<li> I Hacked your UL!</li><li>Dogs are still cute!</li>");
+
+$('#photo').attr('src','https://i.guim.co.uk/img/media/e7fa8affe27ac98750d34d5b062076a5f38ba236/0_0_4182_2510/master/4182.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=70f3ec469c60fa32c73ee8dcef19a828');
+
+$('img').css('width','200px');
+
+$('img').last().attr('src','https://ichef.bbci.co.uk/news/1024/cpsprodpb/11711/production/_100314417_1.jpg');
+
+$('input').val('rusty steele');
