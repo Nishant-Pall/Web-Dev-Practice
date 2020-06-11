d7d4c05df149a60405d83ceb5984dcf99c736134
nishant <palln6935@gmail.com>
2020-06-11 19:20:37 +0530

    condtionals, loops in ejs

--- Intermediate Express/app.js
+++ Intermediate Express/app.js
@@ -12,6 +12,16 @@ app.get('/fallinlovewith/:thing', function(req, res){
 	});
 });
 
+app.get('/posts', function(req, res){
+	var posts =[
+		{title:"Post 1", author:"Susy"},
+		{title:"My Pet bunny", author:"Charlie"},
+		{title:"Cute pomski", author:"Colt"}
+	];
+
+	res.render("posts.ejs", {posts:posts})
+});
+
 
 app.listen(5000, function(){
 	console.log('Server running')
