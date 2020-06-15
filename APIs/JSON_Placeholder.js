e26b0641c4a14a472398c0f58522b4d4e9e8970d
nishant <palln6935@gmail.com>
2020-06-15 19:47:22 +0530

    add locus, ES6 syntax, request-promises

---
+++ APIs/JSON_Placeholder.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+request('https://jsonplaceholder.typicode.com/users/1', (error, response, body)=>{
+	if(!error && response.statusCode == 200){
+		const parsedData = JSON.parse(body);
+		console.log(`${parsedData.name} lives in  ${parsedData.address.city}`);
+	}
+});
+
+// ES6 Syntax below
+
+// const rp = require('request-promise');
+
+// rp('https://jsonplaceholder.typicode.com/users/1')
+// 	.then((body)=>{
+// 		const parsedData = JSON.parse(body);
+// 		console.log(`${parsedData.name} lives in  ${parsedData.address.city}`);
+// 	})
+// 	.catch((err)=>{
+// 		console.log('Error',err);
+// 	})
