103c42dc36e71487a7aa8c07d69585c4117bffc0
nishant <palln6935@gmail.com>
2020-06-16 16:20:55 +0530

    movie app, add basic request route

---
+++ movie_search_app/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+app.set('view engine','ejs');
+
+app.get('/results', function(req, res){
+	request('http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb',function(error, response, body){
+		if(!error && response.statusCode ==200){
+			var results = JSON.parse(body);
+			res.send(results["Search"][0]['Title']);
+		}
+	});
+});
+
+app.listen(5000, function(){
+	console.log("Movie app has started");
+})
