6c9f28683eb7193237d771492986bd2f9bb7f78b
nishant <palln6935@gmail.com>
2020-06-26 20:34:00 +0530

    add retrieving data, saving new instance, handle warnings

--- Mongoose/cats.js
+++ Mongoose/cats.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/cat_app");
+mongoose.connect("mongodb://localhost/cat_app",{useNewUrlParser:true, useUnifiedTopology: true});
 
 
 var catSchema = new mongoose.Schema({
@@ -12,5 +12,42 @@ var Cat = mongoose.model("Cat", catSchema);
 
 // adding a new cat to DB
 
+// var george = new Cat({
+// 	name:"Norris",
+// 	age:7,
+// 	temperament:"Evil"
+// });
+
+// george.save((err, cat)=>{
+// 	if(err){
+// 		console.log("Something went wrong");
+// 	} else{
+// 		console.log("We just saved a cat to the db");
+// 		console.log(cat);
+// 	}
+// });
+
+Cat.create({
+	name:"Snow White",
+	age:15,
+	temperament:"Bland"
+}, (err, cats)=>{
+	if(err){
+		console.log(err);
+	} else{
+		console.log(cats);
+	}
+});
+
 
 // retrieve all cats from the DB
+
+Cat.find({}, (err, cats)=>{
+	if(err){
+		console.log("OH NO ERROR!");
+		console.log(err);
+	} else{
+		console.log("All the Cats...")
+		console.log(cats)
+	}
+});
