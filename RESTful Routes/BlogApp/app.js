dc23c75d2046f26bf74da71a3be1366b28a93a68
nishant <palln6935@gmail.com>
2020-07-04 18:55:16 +0530

    add SHOW route and style show template

--- RESTful Routes/BlogApp/app.js
+++ RESTful Routes/BlogApp/app.js
@@ -24,6 +24,7 @@ app.get("/",(req, res)=>{
 	res.redirect("/blogs")
 })
 
+// INDEX ROUTE
 app.get('/blogs',(req,res)=>{
 	Blog.find({}, (err, blogs)=>{
 		if(err){
@@ -34,7 +35,33 @@ app.get('/blogs',(req,res)=>{
 	})
 })
 
+// NEW ROUTE
+app.get("/blogs/new", (req, res)=>{
+	res.render("new");
+})
+// CREATE ROUTE
+app.post('/blogs', (req, res)=>{
+	// create blog
+	Blog.create(req.body.blog, (err, newBlog)=>{
+		if(err){
+			res.render("new");
+		} else{
+			res.redirect("/blogs")
+		}
+	})
+})
 
+// SHOW ROUTE
+
+app.get("/blogs/:id", (req, res)=>{
+	Blog.findById(req.params.id, (err, foundBlog)=>{
+		if(err){
+			res.redirect("./blogs")
+		} else {
+			res.render("show",{blog: foundBlog})
+		}
+	})
+})
 
 app.listen(3000, ()=>{
 	console.log("Server is running!!")
