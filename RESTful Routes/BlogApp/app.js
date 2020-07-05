0c4237373cd91f9c383ce139fcdd3215fef01a10
nishant <palln6935@gmail.com>
2020-07-05 18:11:12 +0530

    add EDIT and UPDATE routes

--- RESTful Routes/BlogApp/app.js
+++ RESTful Routes/BlogApp/app.js
@@ -1,5 +1,6 @@
 const express = require("express"),
 app = express(),
+methodOverride = require("method-override"),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose")
 
@@ -8,6 +9,7 @@ mongoose.connect("mongodb://localhost/blog_app",{useNewUrlParser: true, useUnifi
 app.set("view engine","ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 // MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -59,6 +61,28 @@ app.get("/blogs/:id", (req, res)=>{
 			res.redirect("./blogs")
 		} else {
 			res.render("show",{blog: foundBlog})
+		}
+	})
+})
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit",(req, res)=>{
+	Blog.findById(req.params.id, (err, foundBlog)=>{
+		if(err){
+			res.redirect("/blogs")
+		} else{
+			res.render("edit",{blog:foundBlog})
+		}
+	})
+})
+
+// UPDATE ROUTE
+app.put("/blogs/:id", (req ,res)=>{
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog)=>{
+		if(err){
+			res.redirect("/blogs")
+		} else{
+			res.redirect("/blogs/" + req.params.id)
 		}
 	})
 })
